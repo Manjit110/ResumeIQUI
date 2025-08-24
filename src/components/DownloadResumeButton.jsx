bcba158a9faf9d307b4ce7bc1d7b337d50e2c2c0
manjit110 <ms6554353@gmail.com>
2025-08-23 22:03:48 -0400

    Fixed Resume Path

--- src/components/DownloadResumeButton.jsx
+++ src/components/DownloadResumeButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function DownloadResumeButton() {
   return (
     <a
-      href="/Manjit_Singh.pdf"
+      href={process.env.PUBLIC_URL + "/Manjit_Singh.pdf"}
       download
       className="bg-blue-700 hover:bg-blue-900 text-white font-semibold py-2 px-4 rounded-xl shadow-lg transition-colors mb-10 mt-6"
     >
